6baf94917a871078258614ef58b417e6945f3612
Dinesh <dineshvasa12@gmail.com>
2026-06-04 18:50:18 +0530

    day-9

---
+++ Terraform/day-9-rds_module/main.tf
@@ -0,0 +1,73 @@
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = "demodb"
+
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class = "db.t3.large"
+  allocated_storage = 5
+
+  db_name  = "demodb"
+  username = "user"
+  port     = "3306"
+
+  iam_database_authentication_enabled = true
+
+  vpc_security_group_ids = ["sg-088f89efaa39c072f"]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  # Enhanced Monitoring - see example for details on how to create the role
+  # by yourself, in case you don't want to create it automatically
+  monitoring_interval    = "30"
+  monitoring_role_name   = "MyRDSMonitoringRole"
+  create_monitoring_role = true
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  # DB subnet group
+  create_db_subnet_group = true
+  subnet_ids             = ["subnet-0f015e728d3e7fe3e", "subnet-0aa7ebea9f6320a0e"]
+
+  # DB parameter group
+  family = "mysql8.0"
+
+  # DB option group
+  major_engine_version = "8.0"
+
+  # Database Deletion Protection
+  deletion_protection = true
+
+  parameters = [
+    {
+      name  = "character_set_client"
+      value = "utf8mb4"
+    },
+    {
+      name  = "character_set_server"
+      value = "utf8mb4"
+    }
+  ]
+
+  options = [
+    {
+      option_name = "MARIADB_AUDIT_PLUGIN"
+
+      option_settings = [
+        {
+          name  = "SERVER_AUDIT_EVENTS"
+          value = "CONNECT"
+        },
+        {
+          name  = "SERVER_AUDIT_FILE_ROTATIONS"
+          value = "37"
+        },
+      ]
+    },
+  ]
+}
